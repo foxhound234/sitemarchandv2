920ed9ca87511da39ce89cee04d40a6b1e1ff313
foxhound234 <morganlb@protonmail.com>
2018-05-28 17:07:19 +0200

    bootstrapv1

--- application/views/admin/ajouterproduit.php
+++ application/views/admin/ajouterproduit.php
@@ -12,7 +12,7 @@
     <script src="main.js"></script>
 </head>
 <body>
-    
+<div class="form-group">
 <h2><?php echo $TitreDeLaPage ?></h2>
 
 <?php
@@ -20,17 +20,17 @@ echo form_open('Admin/ajouterunproduit');
 
  echo form_label('nom du produit :', 'lbllibelle');
 
- echo  form_input('txtlibelle','',array('pattern'=>'[a-zA-Z0-9]+','title'=>'le produit  doit commencer par une lettre', 'required'=>'required')).'<BR>';
+ echo  form_input('txtlibelle','',array('pattern'=>'[a-zA-Z0-9]+','title'=>'le produit  doit commencer par une lettre', 'required'=>'required','class'=>'form-control')).'<BR>';
 
  echo form_label('détail du produit :', 'lbldétail');
 
- echo  form_textarea('txtdetail', '',array('required'=>'required')).'<BR>';
+ echo  form_textarea('txtdetail', '',array('required'=>'required','class'=>'form-control')).'<BR>';
   echo  form_label('prixht :','lblprixht');
-  echo  form_input('txtprixht', '',array('pattern'=>".{0,}",'title'=>'le prix doit est uniquement composé de chiffres','required'=>'required')).'<BR>';
+  echo  form_input('txtprixht', '',array('pattern'=>".{0,}",'title'=>'le prix doit est uniquement composé de chiffres','required'=>'required','class'=>'form-control')).'<BR>';
 
   echo  form_label('tauxtva :', 'lbltva');
 
-  echo  form_input('txttva', '',array('pattern'=>".{0,10}" ,'title'=>'la tva doit est uniquement composé de chiffres','required'=>'required')).'<BR>';
+  echo  form_input('txttva', '',array('pattern'=>".{0,10}" ,'title'=>'la tva doit est uniquement composé de chiffres','required'=>'required','class'=>'form-control')).'<BR>';
 
    echo form_label('image :', 'lblimage');
  
@@ -38,7 +38,7 @@ echo form_open('Admin/ajouterunproduit');
    
    echo form_label('quantité dans le stock', 'lblquantitestock');
 
-   echo form_input(array( 'name'=>'txtquantitestock','type'=>'number', 'min'=>'0','max'=>'1000','step'=>'1','required'=>'required')).'<BR>';
+   echo form_input(array( 'name'=>'txtquantitestock','type'=>'number', 'min'=>'0','max'=>'1000','step'=>'1','required'=>'required','class'=>'form-control')).'<BR>';
 
   echo form_label('dateajout', 'lbldateajout');
   
@@ -49,22 +49,23 @@ echo form_open('Admin/ajouterunproduit');
      '1'=> 'VRAI',
      '0'=> 'FAUX'
   );
-  echo form_dropdown('txtdispo',$liste,'default',array('required'=>'required')).'<BR>';
+  echo form_dropdown('txtdispo',$liste,'default',array('required'=>'required','class'=>'form-control')).'<BR>';
 
  echo form_label('Numero de marque :','lblnomarque');
- echo "<select name='txtNoMarque' id='id' required>";
+ echo "<select name='txtNoMarque' class='form-control' id='id' required>";
      foreach ($LesMarques as $uneMarque) {
          echo "<option value='". $uneMarque['NOMARQUE'] . "'>" . $uneMarque['NOM'] . "</option>";
      }
  echo "</select><br/>";
  echo form_label('Numero de Categorie :','lblnocategorie');
- echo "<select name='txtNoCategorie' id='id' required>";
+ echo "<select name='txtNoCategorie' class='form-control' id='id' required>";
     foreach ($LesCategorie as $uneCategorie) {
         echo "<option value='". $uneCategorie['NOCATEGORIE'] . "'>" . $uneCategorie['LIBELLE'] . "</option>";
     }
 echo "</select><br/>";
-echo form_submit('boutonAjouter', 'ajouter').'<BR>';
+echo form_submit('boutonAjouter', 'ajouter',array('class'=>"btn btn-primary")).'<BR>';
 echo form_close();
 ?>
+</div>
 </body>
 </html>
--- application/views/visiteur/creercompte.php
+++ application/views/visiteur/creercompte.php
@@ -12,7 +12,7 @@
 <body>
     
 <h2><?php echo $TitreDeLaPage ?></h2>
-
+<div class="form-group">
 <?php
 
 echo validation_errors(); // mise en place de la validation
@@ -25,32 +25,33 @@ echo form_open('Visiteur/ajouterunclient');
 
 echo form_label('prenom','lbxPrenom');
 
-echo form_input(array('name'=>'txtPrenom','value'=>'','pattern'=>'[a-zA-Z0-9]+','required'=>'required')).'<BR>';
+echo form_input(array('name'=>'txtPrenom','value'=>'','pattern'=>'[a-zA-Z0-9]+','required'=>'required','class'=>'form-control')).'<BR>';
 
 echo form_label('Nom','lbxNom'); // creation d'un label devant la zone de saisie
 
-echo form_input(array('name'=>'txtNom','value'=>'','pattern'=>'[a-zA-Z0-9]+','required'=>'required')).'<BR>';
+echo form_input(array('name'=>'txtNom','value'=>'','pattern'=>'[a-zA-Z0-9]+','required'=>'required','class'=>'form-control')).'<BR>';
 echo form_label('adresse', 'lbxadresse');
 
-echo form_input(array('name'=>'txtadresse','value'=>'','pattern'=>'[a-zA-Z0-9]+','required'=>'required')).'<BR>';
+echo form_input(array('name'=>'txtadresse','value'=>'','pattern'=>'[a-zA-Z0-9]+','required'=>'required','class'=>'form-control')).'<BR>';
 
 echo form_label('ville', 'lbxVille');
 
-echo form_input(array('name'=>'txtVille','value'=>'','pattern'=>'[a-zA-Z0-9]+','required'=>'required')).'<BR>';
+echo form_input(array('name'=>'txtVille','value'=>'','pattern'=>'[a-zA-Z0-9]+','required'=>'required','class'=>'form-control')).'<BR>';
 echo form_label('codepostal','lbxcodePostal');
 
-echo form_input(array('name'=>'txtcodePostal','value'=>'','pattern'=>'[0-9]','required'=>'required')).'<BR>';
+echo form_input(array('name'=>'txtcodePostal','value'=>'','pattern'=>'[0-9]','required'=>'required','class'=>'form-control')).'<BR>';
 echo form_label('email', 'lbxemail');
 
-echo form_input(array('name'=>'txtEmail','value'=>'','type'=>'email','required'=>'required')).'<BR>';
+echo form_input(array('name'=>'txtEmail','value'=>'','type'=>'email','required'=>'required','class'=>'form-control')).'<BR>';
 
 echo form_label('Mot de passe','lbxMdp');
 
-echo form_password(array('name'=>'txtMdp','value'=>'','pattern'=>'(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$','required'=>'required')).'<BR>';
+echo form_password(array('name'=>'txtMdp','value'=>'','pattern'=>'(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$','required'=>'required','class'=>'form-control')).'<BR>';
 
 echo form_submit('btnEnregistrement', 'enregistrement').'<BR>';
 
 echo form_close();
 ?>
+</div>
 </body>
 </html>
