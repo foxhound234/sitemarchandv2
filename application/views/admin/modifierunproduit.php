1de35f1d657c92ea17ca698a2cb128ea5cde22c8
foxhound234 <morganlb@protonmail.com>
2018-05-29 09:10:15 +0200

    bootstrapv2

--- application/views/admin/modifierunproduit.php
+++ application/views/admin/modifierunproduit.php
@@ -11,23 +11,24 @@
     <script src="main.js"></script>
 </head>
 <body>
+<div class="form-group">
 <h2><?php echo $TitredeLaPage ?></h2>
 <?php
 echo form_open('Admin/ModifierunProduit/'.$Leproduit['NOPRODUIT']);
 
  echo form_label('nom du produit :', 'lbllibelle');
 
- echo  form_input(array('name'=>'txtlibelle','value'=>$Leproduit['LIBELLE'],'pattern'=>'[a-zA-Z0-9]+','title'=>'le produit  doit commencer par une lettre', 'required'=>'required')).'<BR>';
+ echo  form_input(array('name'=>'txtlibelle','value'=>$Leproduit['LIBELLE'],'pattern'=>'[a-zA-Z0-9]+','title'=>'le produit  doit commencer par une lettre', 'required'=>'required','class'=>'form-control')).'<BR>';
 
  echo form_label('détail du produit :', 'lbldétail');
 
- echo  form_textarea(array('name'=>'txtdetail','value'=>$Leproduit['DETAIL'],'required'=>'required')).'<BR>';
+ echo  form_textarea(array('name'=>'txtdetail','value'=>$Leproduit['DETAIL'],'required'=>'required','class'=>'form-control')).'<BR>';
   echo  form_label('prixht :','lblprixht');
-  echo  form_input(array('name'=>'txtprixht','value'=>$Leproduit['PRIXHT'],'pattern'=>".{0,}",'title'=>'le prix doit est uniquement composé de chiffres','required'=>'required')).'<BR>';
+  echo  form_input(array('name'=>'txtprixht','value'=>$Leproduit['PRIXHT'],'pattern'=>".{0,}",'title'=>'le prix doit est uniquement composé de chiffres','required'=>'required','class'=>'form-control')).'<BR>';
 
   echo  form_label('tauxtva :', 'lbltva');
 
-  echo  form_input(array('name'=>'txttva','value'=>$Leproduit['TAUXTVA'],'pattern'=>".{0,10}" ,'title'=>'la tva doit est uniquement composé de chiffres','required'=>'required')).'<BR>';
+  echo  form_input(array('name'=>'txttva','value'=>$Leproduit['TAUXTVA'],'pattern'=>".{0,10}" ,'title'=>'la tva doit est uniquement composé de chiffres','required'=>'required','class'=>'form-control')).'<BR>';
 
    echo form_label('image :', 'lblimage');
  
@@ -35,33 +36,34 @@ echo form_open('Admin/ModifierunProduit/'.$Leproduit['NOPRODUIT']);
    
    echo form_label('quantité dans le stock', 'lblquantitestock');
 
-   echo form_input(array( 'name'=>'txtquantitestock','type'=>'number','value'=>$Leproduit['QUANTITEENSTOCK'],'min'=>'0','max'=>'1000','step'=>'1','required'=>'required')).'<BR>';
+   echo form_input(array( 'name'=>'txtquantitestock','type'=>'number','value'=>$Leproduit['QUANTITEENSTOCK'],'min'=>'0','max'=>'1000','step'=>'1','required'=>'required','class'=>'form-control')).'<BR>';
   
   echo form_label('dateajout', 'lbldateajout');
   
-  echo form_input(array('type'=>'date','name'=>'txtdateajout','value'=>$Leproduit['DATEAJOUT'],'class'=>'clearBtn','required'=>'required')).'<BR>';
+  echo form_input(array('type'=>'date','name'=>'txtdateajout','value'=>$Leproduit['DATEAJOUT'],'class'=>'clearBtn','required'=>'required','class'=>'form-control')).'<BR>';
   
   echo form_label('disponibilité', 'lbldispo');
   $liste=array(
      '1'=> 'VRAI',
      '0'=> 'FAUX'
   );
-  echo form_dropdown('txtdispo',$liste,'default',array('required'=>'required')).'<BR>';
+  echo form_dropdown('txtdispo',$liste,'default',array('required'=>'required','class'=>'form-control')).'<BR>';
 
  echo form_label('Numero de marque :','lblnomarque');
- echo "<select name='txtNoMarque' id='id' required>";
+ echo "<select name='txtNoMarque' class='form-control' id='id' required>";
      foreach ($LesMarques as $uneMarque) {
          echo "<option value='". $uneMarque['NOMARQUE'] . "'>" . $uneMarque['NOM'] . "</option>";
      }
  echo "</select><br/>";
  echo form_label('Numero de Categorie :','lblnocategorie');
-echo "<select name='txtNoCategorie' id='id' required>";
+echo "<select name='txtNoCategorie' class='form-control' id='id' required>";
     foreach ($LesCategorie as $uneCategorie) {
         echo "<option value='". $uneCategorie['NOCATEGORIE'] . "'>" . $uneCategorie['LIBELLE'] . "</option>";
     }
 echo "</select><br/>";
-echo form_submit('btnModifier', 'modifier').'<BR>';
+echo form_submit('btnModifier', 'modifier',array('class'=>'btn btn-primary')).'<BR>';
 echo form_close();
 ?>
+</div>
 </body>
 </html>
--- application/views/templates/entete.php
+++ application/views/templates/entete.php
@@ -1,11 +1,11 @@
 <html>
 <head>
     <title>Blog simple</title>
+    <link rel="stylesheet" href="thecolor.css" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
 </head>
 <body>
-
 <?php if(!is_null($this->session->identifiant)) : ?>
     <?php echo'Utilisateur connecté : <B>'.$this->session->identifiant.'</B>&nbsp;&nbsp;';?>
     <a href="<?php echo site_url('Client/seDeconnecter') ?>">Se déconnecter</a>&nbsp;&nbsp;
@@ -26,19 +26,22 @@
    echo form_submit('btnrecherche','recherché');
    echo form_close();?>
        <?php endif; ?>
-       <?php else : ?>
-        <a href="<?php echo site_url('Client/connexion') ?>">Se Connecter</a>&nbsp;&nbsp;
-        <?php endif; ?>
+       <?php else :?>
+       <a href="<?php echo site_url('Client/connexion') ?>"> Connexion</a>&nbsp;&nbsp;
+       <?php endif; ?>
         <?php if($this->session->profil==null) : ?>
+        <div class="container-fluid,lemenu">
      <a href="<?php echo site_url('Visiteur/afficherlesproduits') ?>">Lister les produits</a>&nbsp;&nbsp;
     <a href="<?php echo site_url('Visiteur/affichelescategorie') ?>">Lister les categories </a>&nbsp;&nbsp;
     <a href="<?php echo site_url('Visiteur/affichagedepanier') ?>">affiché le panier </a>&nbsp;&nbsp;
     <a href="<?php echo site_url('Visiteur/ajouterunclient') ?>"> enregistrement </a>&nbsp;&nbsp;
+    <a href="<?php echo site_url('Client/connexion') ?>">Se Connecter</a>&nbsp;&nbsp;
     <?php
     echo form_open('Visiteur\Recherche');
     echo form_input('txtlibelle','',array('pattern'=>'^[a-zA-Z][a-zA-Z0-9]*','title'=>'le produit  doit commencer par une lettre', 'required'=>'required'));
    echo form_submit('btnrecherche', 'recherché');
    echo form_close();?>
+   </div>
     <?php endif;?>
 </body>
 </html>
